3d98a08d786b05b0e69b65b12ca66a084529e64c
FaithRovina <faithrovina14@gmail.com>
2024-04-13 19:37:44 +0000

    login

--- admin/login/login_check.php
+++ admin/login/login_check.php
@@ -1,17 +1,17 @@
 <?php
-// session_start();
-// include('../../settings/connection.php');
-// include('../../settings/constants.php');
+session_start();
+include('../../settings/connection.php');
+include('../../settings/constants.php');
 
 
-// // Check if the 'username' session variable is set
-// if (!isset($_SESSION['username'])) {
+// Check if the 'username' session variable is set
+if (!isset($_SESSION['username'])) {
     
-//    // User is not logged in
-//     echo "<script>alert('You are not logged in. Please login in to access Admin Panel!');</script>";
-//     // Redirect to the login page
-//     header("Location: " . SITEURL . "login_admin_view.php");
-//     exit();
-// }
+   // User is not logged in
+    echo "<script>alert('You are not logged in. Please login in to access Admin Panel!');</script>";
+    // Redirect to the login page
+    header("Location: " . SITEURL . "login_admin_view.php");
+    exit();
+}
 
 
