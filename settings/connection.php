67599673fc4913e60bfaf61900f6d8e6d62663e7
FaithRovina <faithrovina14@gmail.com>
2024-04-13 14:45:32 +0000

    updated connection

--- settings/connection.php
+++ settings/connection.php
@@ -1,8 +1,8 @@
 <?php
 //constants
-$SERVER= 'localhost:3308';
-$USERNAME= 'root';
-$PASSWORD= '@StrateGicPlan20178##';
+$SERVER= 'farmtofork.mysql.database.azure.com';
+$USERNAME= 'FaithRovina';
+$PASSWORD= '@SkinnyQuee76';
 $DATABASE='food-ordering';
 
 $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE) or die("The database was not created");
