359171a37d127bb01be8f34c5aecb1320866aa1d
FaithRovina <faithrovina14@gmail.com>
2024-04-14 02:31:15 +0000

    admin- index removed moving text

--- admin/index.php
+++ admin/index.php
@@ -38,94 +38,56 @@ $total_revenue = getTotalRevenue();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<style>
-       /* CSS styles for the moving text */
-       #moving-text {
-            position: fixed;
-            bottom: 100px;
-            left: 0;
-            width: 50%;
-            background-color: white;
-            color: black;
-            padding: 25px; 
-            text-align: center;
-            font-size: 30px;
-            
-        }
-    </style>
 </head>
-
-
+<body>
     <div class="main-content">
         <div class="wrapper">
-         <h1> DASHBOARD </h1> 
+            <h1> DASHBOARD </h1> 
 
-         <div class="col-4 text-center">
-         <h1><?php echo $total_categories; ?></h1>
-            <br/>
-            Categories
-        </div>
+            <div class="col-4 text-center">
+                <h1><?php echo $total_categories; ?></h1>
+                <br/>
+                Categories
+            </div>
 
-        <div class="col-4 text-center">
-            <h1><?php echo $total_foods; ?></h1>
-            <br/>
-            Foods
-        </div>
+            <div class="col-4 text-center">
+                <h1><?php echo $total_foods; ?></h1>
+                <br/>
+                Foods
+            </div>
 
-        <div class="col-4 text-center">
-            <h1><?php echo $total_orders; ?></h1>
-            <br/>
-            Total Orders
-        </div>
+            <div class="col-4 text-center">
+                <h1><?php echo $total_orders; ?></h1>
+                <br/>
+                Total Orders
+            </div>
 
-        <div class="col-4 text-center">
-            <h1><?php echo $processing_orders; ?></h1>
-            <br/>
-            Processing Orders
-        </div>
+            <div class="col-4 text-center">
+                <h1><?php echo $processing_orders; ?></h1>
+                <br/>
+                Processing Orders
+            </div>
 
-        <div class="col-4 text-center">
-            <h1><?php echo $delivered_orders; ?></h1>
-            <br/>
-            Delivered Orders
-        </div>
+            <div class="col-4 text-center">
+                <h1><?php echo $delivered_orders; ?></h1>
+                <br/>
+                Delivered Orders
+            </div>
 
-        <div class="col-4 text-center">
-            <h1><?php echo $cancelled_orders; ?></h1>
-            <br/>
-            Cancelled Orders
-        </div>
+            <div class="col-4 text-center">
+                <h1><?php echo $cancelled_orders; ?></h1>
+                <br/>
+                Cancelled Orders
+            </div>
 
-        <div class="col-4 text-center">
-            <h1>$<?php echo $total_revenue; ?></h1>
-            <br/>
-            Total Revenue
-        </div>
+            <div class="col-4 text-center">
+                <h1>$<?php echo $total_revenue; ?></h1>
+                <br/>
+                Total Revenue
+            </div>
 
-        <div class="clearfix"> </div>
+            <div class="clearfix"> </div>
         </div>
     </div>
-
-    <div id="moving-text"></div>
-    <script>
-        // JavaScript code to display the moving text with typing effect
-        var quote = "Finding the happiness and finding the satisfaction in continuously serving somebody else something good to eat is what makes a really good restaurant. — Mario Batali";
-        var speed = 100;
-        var index = 0;
-        var typingTimer;
-
-        function typeText() {
-            document.getElementById("moving-text").innerHTML += quote.charAt(index);
-            index++;
-            if (index < quote.length) {
-                typingTimer = setTimeout(typeText, speed);
-            }
-        }
-
-        
-        window.onload = function() {
-            typeText();
-        };
-    </script>
-    </body>
+</body>
 </html>
